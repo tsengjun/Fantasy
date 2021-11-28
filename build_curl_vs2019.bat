3768a7bcedd6e10de8023e01929c4fbf7a2e0862
Jun Tseng <junzeng@opencpu.com>
2021-11-28 09:49:53 +0800

    Build library with /Zi by default

--- build_curl_vs2019.bat
+++ build_curl_vs2019.bat
@@ -11,6 +11,7 @@ echo %PATH%
 call buildconf.bat
 
 cd winbuild
+perl -pi.bak -e "s/\/Z7/\/Zi/gi" MakefileBuild.vc
 nmake /f Makefile.vc mode=static DEBUG=yes MACHINE=x64 GEN_PDB=yes RTLIBCFG=static ENABLE_UNICODE=yes ENABLE_IPV6=no ENABLE_SSPI=no ENABLE_SCHANNEL=no ENABLE_IDN=yes WITH_SSL=static SSL_PATH=%OPENSSL_ROOT_DIR%\build
 cd %CWD%
 
