8a342c20b81f478d8804741a24a996324681da41
Jun Tseng <junzeng@opencpu.com>
2021-11-30 22:18:05 +0800

    Add files via upload

---
+++ build_libssh2_vs2019.bat
@@ -0,0 +1,22 @@
+@echo on
+
+:: vcvars64.bat ::
+set CWD=%~sdp0
+cd /d "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
+call vcvars64.bat
+cd /d %CWD%
+
+:: path ::
+set PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64";"C:\Program Files\NASM";"C:\Strawberry\perl\bin";%PATH%
+echo %PATH%
+
+rmdir /S /Q build
+mkdir build
+
+cd build
+
+cmake ../ -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DCLEAR_MEMORY:BOOL=ON -DCMAKE_CONFIGURATION_TYPES:STRING=Debug -DCMAKE_C_FLAGS_DEBUG:STRING="/MTd /Zi /Ob0 /Od /RTC1 /guard:cf" -DCRYPTO_BACKEND:STRING=OpenSSL -DENABLE_DEBUG_LOGGING:BOOL=OFF
+perl -pi.bak -e "s/MultiByte/Unicode/gi" src\libssh2.vcxproj
+msbuild libssh2.sln
+
+cd %CWD%
